1763022729f244ad06afe257ccbf231d2d46bbfc
nr <99275945+giterhaber@users.noreply.github.com>
2022-12-10 15:53:55 +0800

    entry 0

--- main.js
+++ main.js
@@ -62,7 +62,12 @@ import "https://cdn.skypack.dev/firebase/compat/firestore";
 
     })
       var entryCount =   $('.new-entry').find('.data-content').length
-      notify(entryCount + ' new entry!', 'click to open', 'https://i.pinimg.com/236x/9f/ee/55/9fee55fe5559da752bd2851103b9f69b.jpg')
+      
+      if(entryCount === 0) {
+        console.log('0')
+      } else {
+        notify(entryCount + ' new entry!', 'click to open', 'https://i.pinimg.com/236x/9f/ee/55/9fee55fe5559da752bd2851103b9f69b.jpg')
+      }
 
 });
 
