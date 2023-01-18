ae82dcd0e06997d880715470b9a63e1b7221f4ca
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-19 01:37:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import {notify} from 'https://giterhaber.github.io/web-codes/notification.js'
-import {cmBwsbFSqC as config} from 'https://giterhaber.github.io/web-codes/configs.js'
+// import {cmBwsbFSqC as config} from 'https://giterhaber.github.io/web-codes/configs.js'
 
 import firebase from "https://cdn.skypack.dev/firebase/compat/app";
 import "https://cdn.skypack.dev/firebase/compat/auth";
 import "https://cdn.skypack.dev/firebase/compat/firestore";
+
+const config = {
+  apiKey: "AIzaSyDAk6wPmjinvA2AaKkBpXFpOkEwXssluvc",
+  authDomain: "chainresolver.firebaseapp.com",
+  projectId: "chainresolver",
+  storageBucket: "chainresolver.appspot.com",
+  messagingSenderId: "28247484124",
+  appId: "1:28247484124:web:af0fc65ac1c51600b1b619",
+  measurementId: "G-4FRHM8ZK36"
+}
 
   firebase.initializeApp(config);
   const db = firebase.firestore();
