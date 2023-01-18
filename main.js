3af1917cd89d69b6eac3c045088db32959d9fae0
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-19 01:20:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -112,13 +112,13 @@ firebase.auth().signOut();
 
 }
 
- firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      // ...
-    } else {
-      location.href = "/";
-    }
-  });
+//  firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/firebase.User
+//       var uid = user.uid;
+//       // ...
+//     } else {
+//       location.href = "/";
+//     }
+//   });
